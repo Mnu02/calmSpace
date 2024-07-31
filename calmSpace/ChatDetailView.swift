2298da78dae692c622cfcaf12bfc6e86a3a72ba2
Mnumzana Moyo <mfm222@cornell.edu>
2024-07-31 15:24:13 -0400

    Implement logic for sending message and UI

--- calmSpace/ChatDetailView.swift
+++ calmSpace/ChatDetailView.swift
@@ -6,137 +6,106 @@
 //
 
 import SwiftUI
-import OpenAISwift
+import OpenAI
 
-final class ViewModel: ObservableObject {
-    init() {}
+class ViewModel: ObservableObject {
+    @Published var messages: [Message] = [.init(content: "Hello", isUser: true, timestamp: Date()), .init(content: "Hi", isUser: false, timestamp: Date())]
     
-    private var client: OpenAISwift?
     
-    func setup() {
-        client = OpenAISwift(config: OpenAISwift.Config.makeDefaultOpenAI(apiKey:"sk-proj-t3OqwcRo9lRvwAZRMf3dT3BlbkFJUDUOMYQcfqgkf9cM1TvI"))
+    
+    func sendNewMessage(content :  String) {
+        let userMessage = Message(content: content, isUser: true, timestamp: Date())
+        self.messages.append(userMessage)
+        getBotReply()
     }
     
-    func send(text: String, completion: @escaping (String) -> Void) {
-        client?.sendCompletion(with: text, maxTokens: 500, completionHandler: { result in
+    func getBotReply() {
+        let query = ChatQuery(
+            messages: self.messages.map({
+                .init(role: .user, content: $0.content)!
+            }),
+            model: .gpt3_5Turbo
+        )
+        
+        openAI.chats(query: query) { result in
             switch result {
-            case .success (let model):
-                let output = model.choices?.first?.text ?? ""
-                completion(output)
-            case .failure (let error):
-                print("Error:\(error.localizedDescription)")
+            case .success(let success):
+                guard let choice = success.choices.first else {
+                    return
+                }
+                guard let message = choice.message.content?.string else { return }
+                DispatchQueue.main.async {
+                    self.messages.append(Message(content: message, isUser: false, timestamp: Date()))
+                }
+            case .failure(let failure):
+                print(failure)
             }
-        })
+        }
     }
 }
 
 struct ChatDetailView: View {
-    
-    var chat: Chat
-    @State private var messages: [Message] = []
-    @State private var messageText: String = ""
-    @ObservedObject private var messageService = MessageService()
-    @ObservedObject private var viewModel = ViewModel()
-    
+    @StateObject var chatController: ViewModel = .init()
+    @State var string: String = ""
     var body: some View {
+        
+        Divider()
+        
         VStack {
-            
-            Rectangle()
-                .frame(maxWidth: .infinity, minHeight: 1, maxHeight: 1)
-                .foregroundColor(.deepPurple)
-                .padding(1)
-
-            
             ScrollView {
-                VStack(spacing: 10) {
-                    ForEach(messages) { message in
-                        HStack {
-                            if message.isUser {
-                                Spacer()
-                                Text(message.text)
-                                    .padding()
-                                    .background(Color.deepPurple.opacity(0.8))
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                                    .frame(maxWidth: UIScreen.main.bounds.width * 0.7, alignment: .trailing)
-                            } else {
-                                Text(message.text)
-                                    .padding()
-                                    .background(Color.gray.opacity(0.2))
-                                    .foregroundColor(.black)
-                                    .cornerRadius(10)
-                                    .frame(maxWidth: UIScreen.main.bounds.width * 0.7, alignment: .leading)
-                                Spacer()
-                            }
-                        }
-                    }
+                ForEach(chatController.messages) {
+                    message in
+                    MessageView(message: message)
+                        .padding(5)
                 }
-                .padding()
             }
+            
+            Divider()
             
             HStack {
-                Button(action: startVoiceRecording) {
-                    Image(systemName: "mic.fill")
-                        .foregroundColor(.white)
-                }
-                
-                TextField("Type a message...", text: $messageText)
-                    .padding()
-                    .frame(height: 35)
-                    .background(.white)
-                    .cornerRadius(10)
-                    
-                Button(action: sendMessage) {
-                    Image(systemName: "paperplane.fill")
-                        .foregroundColor(.white)
-                        .padding(.leading, 5)
+                TextField("Message...", text: self.$string, axis: .vertical)
+                    .padding(5)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(15)
+                Button {
+                    self.chatController.sendNewMessage(content: string)
+                    string = ""
+                } label: {
+                    Image(systemName: "paperplane")
                 }
             }
-            .padding(10)
-            .padding(.vertical, 10)
-            .frame(maxWidth: .infinity)
-            .frame(height: 40)
-            .background(Color.deepPurple)
+            .padding()
         }
-        .navigationTitle(chat.title)
-        .onAppear {
-            viewModel.setup()
-            messageService.fetchMessages { error in
-                if let error = error {
-                    print("Error fetching messages: \(error.localizedDescription)")
-                }
-            }
-        }
-    }
-    
-    
-    
-    func sendMessage() {
-        guard !messageText.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-        let userMessage = Message(id: UUID().uuidString, text: messageText, isUser: true, timestamp: Date())
-        messages.append(userMessage)
-        
-        viewModel.send(text: messageText) { response in
-                    let botMessage = Message(id: UUID().uuidString, text: response, isUser: false, timestamp: Date())
-                    print("GPT ANSWER: " , response)
-                    DispatchQueue.main.async {
-                        messages.append(botMessage)
-                    }
-                }
-                
-                messageText = ""
-    }
-    
-    func startVoiceRecording() {
-        // Add voice recording logic here
     }
 }
 
-struct ChatDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        let sampleChat = Chat(id: "123", title: "My lifestyle", content: "Had a good time yesterday at the park", userId: "Mnumzana")
-        NavigationView {
-            ChatDetailView(chat: sampleChat)
+struct MessageView: View {
+    var message: Message
+    var body: some View {
+        Group {
+            if message.isUser {
+                HStack {
+                    Spacer()
+                    Text(message.content)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .clipShape(Capsule())
+                }
+            } else {
+                HStack {
+                    Text(message.content)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(Color.white)
+                        .clipShape(Capsule())
+                    Spacer()
+                }
+            }
         }
     }
+}
+
+#Preview {
+    ChatDetailView()
 }
