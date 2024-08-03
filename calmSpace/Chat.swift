02a78de4a81e9d5259818e5ab26634002143a7c6
Mnumzana Moyo <mfm222@cornell.edu>
2024-08-03 18:33:08 -0400

    Change the init for Chat Method

--- calmSpace/Chat.swift
+++ calmSpace/Chat.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 
-struct Chat: Identifiable, Decodable, Hashable {
-    var id: String?
+struct Chat: Identifiable, Codable, Hashable {
+    var id: String
     var title: String
     var content: String
     var userId: String
+    
+    // Optionally, you can add an initializer if needed
+    init(id: String = UUID().uuidString, title: String, content: String, userId: String) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.userId = userId
+    }
 }
+
